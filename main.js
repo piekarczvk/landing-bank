54a52a715c58d885376863b4e10fb47d2f6eea41
Guillaume MILON <guillaume@thegivingmovement.com>
2023-07-26 16:46:05 +0400

    hero: fix design

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import "./style.css"
-
-const $card = document.querySelector('.card');
+const card = document.querySelector(".card");
 let bounds;
 
 function rotateToMouse(e) {
@@ -10,38 +8,38 @@ function rotateToMouse(e) {
   const topY = mouseY - bounds.y;
   const center = {
     x: leftX - bounds.width / 2,
-    y: topY - bounds.height / 2
-  }
-  const distance = Math.sqrt(center.x**2 + center.y**2);
-  
-  $card.style.transform = `
-    scale3d(1.07, 1.07, 1.07)
-    rotate3d(
-      ${center.y / 100},
-      ${-center.x / 100},
-      0,
-      ${Math.log(distance)* 2}deg
-    )
-  `;
-  
-  $card.querySelector('.glow').style.backgroundImage = `
-    radial-gradient(
-      circle at
-      ${center.x * 2 + bounds.width/2}px
-      ${center.y * 2 + bounds.height/2}px,
-      #ffffff55,
-      #0000000f
-    )
-  `;
+    y: topY - bounds.height / 2,
+  };
+  const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+  card.style.transform = `
+              scale3d(1.07, 1.07, 1.07)
+              rotate3d(
+                ${center.y / 100},
+                ${-center.x / 100},
+                0,
+                ${Math.log(distance) * 2}deg
+              )
+            `;
+
+  card.querySelector(".glow").style.backgroundImage = `
+              radial-gradient(
+                circle at
+                ${center.x * 2 + bounds.width / 2}px
+                ${center.y * 2 + bounds.height / 2}px,
+                rgba(255, 255, 255, 0.33),
+                rgba(0, 0, 0, 0.06)
+              )
+            `;
 }
 
-$card.addEventListener('mouseenter', () => {
-  bounds = $card.getBoundingClientRect();
-  document.addEventListener('mousemove', rotateToMouse);
+card.addEventListener("mouseenter", () => {
+  bounds = card.getBoundingClientRect();
+  document.addEventListener("mousemove", rotateToMouse);
 });
 
-$card.addEventListener('mouseleave', () => {
-  document.removeEventListener('mousemove', rotateToMouse);
-  $card.style.transform = '';
-  $card.style.background = '';
+card.addEventListener("mouseleave", () => {
+  document.removeEventListener("mousemove", rotateToMouse);
+  card.style.transform = "";
+  card.querySelector(".glow").style.backgroundImage = "";
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-  ],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#121139",
+        "text-light": "#F8F8F8",
+        "pink-light": "#9130F4",
+        "blue-light": "#4646F9",
+      },
+    },
   },
   plugins: [],
-}
-
+};
